ec45d7f8e32118dd991e48a4c3abd526107789e3
sunil <sunilkunwal97@gmail.com>
2024-03-26 18:11:23 +0530

    java

---
+++ src/coreJava/MethodsDemo2.java
@@ -0,0 +1,12 @@
+package coreJava;
+
+public class MethodsDemo2 {
+	
+	
+public String getUserData() {
+		
+		System.out.println("hello World");
+		return "sunil kunwal";
+	}
+
+}
--- src/coreJava/StringsInJava.java
+++ src/coreJava/StringsInJava.java
@@ -4,7 +4,31 @@ public class StringsInJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+	//String is an object 		//String literal
+		
+		//String s1 = "Sunil Kunwal";
+		String s2 = "hello";
+		
+		//new 
+		String s3 = new String("Welcome");
+		String s4 = new String ("Welcome");
+		
+		
+		String s = "Sunil Kunwal QA"; 
+		String[] splittrdString = s.split("Kunwal");
+		System.out.println(splittrdString[0]);
+		System.out.println(splittrdString[1]);
+		//System.out.println(splittrdString[2]);
+		System.out.println(splittrdString[1].trim());
+		
+		for (int i=s.length()-1; i>=0; i--) {
+			
+			
+			System.out.println(s.charAt(i));
+		}
+		
 	}
 
 }
